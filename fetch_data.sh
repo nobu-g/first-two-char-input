012311c16dd11c9553d009ba57071b36a15216eb
nobu-g <ueda-n127@hotmail.co.jp>
2019-06-14 21:52:23 +0900

    add data fetch script

---
+++ fetch_data.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -exu
+
+base_dir=/mnt/maple/kawahara/WWW.en/select3/txt_uniq
+for (( i = 0; i < 10800; ++i )); do
+    number=$(printf "%05d" ${i})
+    file_path=${base_dir}/${number}.txt.gz
+    if [[ $(gzcat -f ${file_path} | wc -l) -gt 100 ]]; then
+        gzcat ${file_path} | shuf | tee >(head -n 90 >> train.txt) | tail -n 10 >> test.txt
+    fi
+
+done
